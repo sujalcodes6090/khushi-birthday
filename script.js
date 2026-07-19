7765c30e07887bf18814f54f0f04cd2f3f283804
Sujal Yadav <sujal6090@gmail.com>
2026-07-19 10:47:40 +0530

    Added fireworks

--- script.js
+++ script.js
@@ -36,6 +36,10 @@ btn.addEventListener("click", () => {
             gallery.classList.add("show");
 
             createHearts();
+            createFireworks();
+            setTimeout(createFireworks, 700);
+setTimeout(createFireworks, 1400);
+setTimeout(createFireworks, 2100);
 
         }
 
@@ -61,6 +65,28 @@ function createHearts(){
             heart.remove();
         },6000);
 
+    }
+
+}
+function createFireworks() {
+
+    for (let i = 0; i < 25; i++) {
+
+        const firework = document.createElement("div");
+
+        firework.className = "firework";
+
+        firework.style.left = Math.random() * 100 + "vw";
+        firework.style.top = Math.random() * 70 + "vh";
+
+        firework.style.background =
+            `hsl(${Math.random() * 360},100%,60%)`;
+
+        document.body.appendChild(firework);
+
+        setTimeout(() => {
+            firework.remove();
+        }, 1200);
     }
 
 }
